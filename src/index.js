3d2bf85f3eb216727bbc38a6fbcf445f01ca0f5a
nylltz <nylltz@gmail.com>
2020-01-13 01:51:38 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Just a test for load process.env
+if (process.env.ENV1) {
+  window.analytics.load(process.env.ENV1);
+}
+// Test end
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
